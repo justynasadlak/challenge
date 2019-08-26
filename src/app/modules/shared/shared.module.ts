e0d5e20f2e720e43057507ec214789dd428d7496
Unknown <justynasadlak10@gmail.com>
2019-08-26 09:59:50 +0200

    Create toolbar component

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {CustomMaterialModule} from './modules/custom-material/custom-material.module';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ToolbarComponent],
   imports: [
     CommonModule,
     CustomMaterialModule
