ae5a052c352ad6a29c2991cc225d43a5f94c4922
Unknown <justynasadlak10@gmail.com>
2019-08-26 11:25:39 +0200

    Create top image

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -3,15 +3,16 @@ import { CommonModule } from '@angular/common';
 import {CustomMaterialModule} from './modules/custom-material/custom-material.module';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { TopImageComponent } from './components/top-image/top-image.component';
 
 
 
 @NgModule({
-  declarations: [ToolbarComponent, MenuComponent],
+  declarations: [ToolbarComponent, MenuComponent, TopImageComponent],
   imports: [
     CommonModule,
     CustomMaterialModule
   ],
-  exports: [CustomMaterialModule, ToolbarComponent, MenuComponent]
+  exports: [CustomMaterialModule, ToolbarComponent, MenuComponent, TopImageComponent]
 })
 export class SharedModule { }
