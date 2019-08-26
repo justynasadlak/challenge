9cf59ca2d5864cc7c82ac14aa372bc373e56afa4
Unknown <justynasadlak10@gmail.com>
2019-08-26 09:55:09 +0200

    Add import and export custom material module in shared module

---
+++ src/app/modules/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {CustomMaterialModule} from './modules/custom-material/custom-material.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CustomMaterialModule
+  ],
+  exports: [CustomMaterialModule]
+})
+export class SharedModule { }
