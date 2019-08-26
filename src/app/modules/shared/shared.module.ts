b0ad8548e83c4654bb47f214627fe1197b644dff
Unknown <justynasadlak10@gmail.com>
2019-08-26 10:38:35 +0200

    Create menu component

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {CustomMaterialModule} from './modules/custom-material/custom-material.module';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 
 
 @NgModule({
-  declarations: [ToolbarComponent],
+  declarations: [ToolbarComponent, MenuComponent],
   imports: [
     CommonModule,
     CustomMaterialModule
   ],
-  exports: [CustomMaterialModule, ToolbarComponent]
+  exports: [CustomMaterialModule, ToolbarComponent, MenuComponent]
 })
 export class SharedModule { }
