0d47051ee9468fd4315f54eb121c2a553a0c1fef
Unknown <justynasadlak10@gmail.com>
2019-08-26 14:16:31 +0200

    Remove custom material module

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {CustomMaterialModule} from './modules/custom-material/custom-material.module';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { TopImageComponent } from './components/top-image/top-image.component';
@@ -10,9 +9,8 @@ import { TopImageComponent } from './components/top-image/top-image.component';
 @NgModule({
   declarations: [ToolbarComponent, MenuComponent, TopImageComponent],
   imports: [
-    CommonModule,
-    CustomMaterialModule
+    CommonModule
   ],
-  exports: [CustomMaterialModule, ToolbarComponent, MenuComponent, TopImageComponent]
+  exports: [ToolbarComponent, MenuComponent, TopImageComponent]
 })
 export class SharedModule { }
